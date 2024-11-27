f3267cba178e2db0c05a5d143b72dbc9602fe28a
Kharristov <annawey486@gmail.com>
2024-11-27 18:33:40 +0700

    Добавьте файлы проекта.

---
+++ 5c2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int b; 
+    string N; 
+    cin >> b >> N;
+
+    int decimalValue = 0; 
+
+    // Обходим каждую цифру числа N
+    for (int i = 0; i < N.length(); i++) {
+        char digit = N[i];
+        int value;
+
+        // Определяем числовое значение символа
+        if (isdigit(digit)) {
+            value = digit - '0'; 
+        }
+        else {
+            value = digit - 'a' + 10; 
+        }
+
+        decimalValue = decimalValue * b + value;
+    }
+
+    cout << decimalValue;
+
+    return 0;
+}
